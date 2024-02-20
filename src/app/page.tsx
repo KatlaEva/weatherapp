1a6d77b8fc2f73545ebb40634128cdb7bb4ee64c
KatlaEva <katlaevavik@gmail.com>
2024-02-20 13:10:33 +0000

    Added a Navbar that will hold a search bar later

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
- <div>
-  <h1>Hi app</h1>
+ <div className="flex flex-col gap-4 bg-gray-100 min-h-screen ">
+  <Navbar/>
  </div>
   )
 }
